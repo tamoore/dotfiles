fdef992bde1d342fea8674eaf7cfbaf3062f5151
Todd Moore <11851685+tamoore@users.noreply.github.com>
2020-09-06 23:31:34 +1000

    Disable linux brew install

--- containers/setup.bash
+++ containers/setup.bash
@@ -4,10 +4,10 @@ declare WATCH_EXEC_VERSION="1.14.0"
 
 # Install homebrew for some packages that use homebrew instead of apt or some
 # other linux package manager
-if [ ! -x "$(command -v brew)" ]; then
-    echo -e "\u001b[33m==> Installing homebrew on linux\u001b[0m"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
+# if [ ! -x "$(command -v brew)" ]; then
+#     echo -e "\u001b[33m==> Installing homebrew on linux\u001b[0m"
+#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# fi
 
 # shellcheck disable=SC2016
 echo 'export GPG_TTY=$(tty)' >>"$HOME/.profile"
