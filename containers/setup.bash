f4a5cfa6a93afe1c75069d47f239ae8af030730d
Todd Moore <11851685+tamoore@users.noreply.github.com>
2020-09-06 23:05:42 +1000

    Add brew back to install

--- containers/setup.bash
+++ containers/setup.bash
@@ -2,16 +2,15 @@
 
 # Install homebrew for some packages that use homebrew instead of apt or some
 # other linux package manager
-# if [ ! -x "$(command -v brew)" ]; then
-#    echo -e "\u001b[33m==> Installing homebrew on linux\u001b[0m"
-#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# fi
+if [ ! -x "$(command -v brew)" ]; then
+    echo -e "\u001b[33m==> Installing homebrew on linux\u001b[0m"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
 # shellcheck disable=SC2016
 echo 'export GPG_TTY=$(tty)' >>"$HOME/.profile"
 
+# Setup gpg to work with containers, although at the moment they don't actually
+# work with them which will need to be looked at.
 mkdir -p "$HOME/.gnupg"
 echo 'pinentry-program /usr/bin/pinentry-tty' >"$HOME/.gnupg/gpg-agent.conf"
-#echo 'allow-loopback-pinentry' >>"$HOME/.gnupg/gpg-agent.conf"
-#echo 'use-agent' >"$HOME/.gnupg/gpg.conf"
-#echo 'pinentry-mode loopback' >>"$HOME/.gnupg/gpg.conf"
