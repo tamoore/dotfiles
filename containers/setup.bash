b91ff88008b7459fcc2ff5c5b7534df84bcb5319
Todd Moore <11851685+tamoore@users.noreply.github.com>
2021-02-14 05:44:50 +0000

    ContainerScripts: Ensure errors exit 1

--- containers/setup.bash
+++ containers/setup.bash
@@ -1,30 +1,48 @@
 #!/usr/bin/env bash
+set -e
 
 declare WATCH_EXEC_VERSION="1.14.0"
 
-# Update apt
-sudo apt -y update
+update_apt() {
+    # Update apt
+    sudo apt -y update
+}
 
-# Download watch exec deb
-wget "https://github.com/watchexec/watchexec/releases/download/${WATCH_EXEC_VERSION}/watchexec-${WATCH_EXEC_VERSION}-x86_64-unknown-linux-gnu.deb" -O "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
+install_programs() {
+    # Download watch exec deb
+    wget "https://github.com/watchexec/watchexec/releases/download/${WATCH_EXEC_VERSION}/watchexec-${WATCH_EXEC_VERSION}-x86_64-unknown-linux-gnu.deb" -O "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
 
-# Install watchexec
-sudo apt install "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
+    # Install watchexec
+    sudo apt install "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
 
-# Install vim
-sudo apt install -y vim
+    # Install vim
+    sudo apt install -y vim
 
-# Ensure that the dotfiles gitconfig is included
-git config --global include.path "$HOME/dotfiles/git/.gitconfig"
+    # Install stow
+    sudo apt install -y stow
 
-# Install stow
-sudo apt install -y stow
+    # Add git extras
+    sudo apt install -y git-extras
 
-# Copy profile to $HOME
-cp "$HOME/dotfiles/containers/.profile" "$HOME/"
+    # Get shellcheck
+    sudo apt install -y shellcheck
+}
 
-# Ensure that local is added
-stow -d "$HOME/dotfiles" .local
+configure_environment() {
+    # Ensure that the dotfiles gitconfig is included
+    git config --global include.path "$HOME/dotfiles/git/.gitconfig"
 
-# Add git extras
-sudo apt install -y git-extras
+    # Copy profile to $HOME
+    cp "$HOME/dotfiles/containers/.profile" "$HOME/"
+
+    # Ensure that local is added
+    stow -d "$HOME/dotfiles" .local
+}
+
+main() {
+    update_apt
+    install_programs
+    configure_environment
+}
+
+main "$@"
