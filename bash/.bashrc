685e5a619a69ccbef04c3a497adb62e2f0f137a9
Todd Moore <11851685+tamoore@users.noreply.github.com>
2021-03-04 14:57:05 +1100

    Update of my bash to be relative to $HOME

--- bash/.bashrc
+++ bash/.bashrc
@@ -1,4 +1,4 @@
-#!/usr/bin/bash env
+#!/usr/bin/env bash
 # ~/.bashrc: executed by bash(1) for non-login shells.
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
--- containers/setup.bash
+++ containers/setup.bash
@@ -35,6 +35,9 @@ configure_environment() {
 
     # Ensure that local is added
     stow -d "$HOME/dotfiles" .local
+
+    # Add bashrc
+    ln -sf "$HOME/dotfiles/bash/.oh-my-bash.rc" "$HOME/.bashrc"
 }
 
 main() {
