64e5425c99a19b808001ad47010118ea850504c8
Todd Moore <11851685+tamoore@users.noreply.github.com>
2021-04-13 10:25:02 +1000

    Install ripgrep from deb package

--- setup/containers.bash
+++ setup/containers.bash
@@ -3,6 +3,7 @@ set -e
 
 declare -xg OSH_THEME="brainy"
 declare WATCH_EXEC_VERSION="1.14.0"
+declare RIPGREP_VERSION="12.1.1"
 
 update_apt() {
     # Update apt
@@ -21,6 +22,7 @@ install_programs() {
 
     # Download watch exec deb
     wget "https://github.com/watchexec/watchexec/releases/download/${WATCH_EXEC_VERSION}/watchexec-${WATCH_EXEC_VERSION}-x86_64-unknown-linux-gnu.deb" -O "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
+    wget "https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/ripgrep_${RIPGREP_VERSION}_amd64.deb" -O "$HOME/ripgrep-${RIPGREP_VERSION}.deb"
 
     # Remove oh my bash
     rm -rf $HOME/.bash_it
@@ -30,11 +32,14 @@ install_programs() {
     # Install watchexec
     apt-get install "$HOME/watchexec-${WATCH_EXEC_VERSION}.deb"
 
+    # Install ripgrep
+    apt-get install "$HOME/ripgrep-${RIPGREP_VERSION}.deb"
+
     # Install git extras
     install_git_extras
 
     # Install programs
-    apt-get install -y neovim stow shellcheck git bash-completion parallel ripgrep
+    apt-get install -y neovim stow shellcheck git bash-completion parallel
 }
 
 configure_environment() {
